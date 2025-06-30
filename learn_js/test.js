6b2e3666716b36f54a224cc0b0eff57e40c1239c
lxy123hh <1677837750@qq.com>
2025-06-30 16:19:52 +0800

    feat:test

--- learn_js/test.js
+++ learn_js/test.js
@@ -1,2 +1,6 @@
-let map = new Map();
+let map = new Map([
+  ['name', 'Tom'],
+  ['age', 25],
+  ['job', 'developer']
+]);
 console.log(map); // This will throw an error because 'map' is not defined in this scope
