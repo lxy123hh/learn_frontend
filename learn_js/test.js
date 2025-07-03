40141fb2ebd812f5440525df0b0880b62bd7d192
lxy123hh <1677837750@qq.com>
2025-07-03 15:22:11 +0800

    feat:update test

--- learn_js/test.js
+++ learn_js/test.js
@@ -1,8 +1,22 @@
-function outFunction() {
-  let a = 10;
-  console.log(`a is ${a}`);
-  function innerFunction() {
-    console.log(a);}
-  return innerFunction;
+function createCounter() {
+  let count = 0;  // 私有变量
+  return {
+    increment: () => {
+      count++;
+      console.log(count);
+    },
+    decrement: function() {
+      count--;
+      console.log(count);
+    },
+    getCount: function() {
+      return count;
+    }
+  };
 }
-outFunction();
+
+const counter = createCounter();
+counter.increment();  // 输出: 1
+counter.increment();  // 输出: 2
+counter.decrement();  // 输出: 1
+console.log(counter.getCount());  // 输出: 1
