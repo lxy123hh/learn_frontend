af883b2b60b24e6707c7b6c9ce60e0facce85bb4
lxy123hh <1677837750@qq.com>
2025-07-13 15:23:14 +0800

    feat:vue_first

---
+++ learn_vue/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>{{ test_message }}</h1>
+    <button @click="click_button">点我</button>
+  </div>
+</template>
+
+<script setup>
+import {ref} from 'vue';
+const test_message = ref('lxy');
+function click_button() {
+  test_message.value = '你点我干嘛？';
+}
+
+</script>
